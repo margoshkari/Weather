252d23e1525cdc9fde8a8bc942befd8f81612647
margo <margoshka.ri6@gmail.com>
2021-11-23 18:09:28 +0200

    added changing cities

--- Weather/Form1.Designer.cs
+++ Weather/Form1.Designer.cs
@@ -49,19 +49,17 @@ namespace Weather
             this.tempLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
             this.tempLabel.Location = new System.Drawing.Point(14, 94);
             this.tempLabel.Name = "tempLabel";
-            this.tempLabel.Size = new System.Drawing.Size(42, 46);
+            this.tempLabel.Size = new System.Drawing.Size(0, 46);
             this.tempLabel.TabIndex = 0;
-            this.tempLabel.Text = "0";
             // 
             // cityLabel
             // 
             this.cityLabel.AutoSize = true;
-            this.cityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.cityLabel.Location = new System.Drawing.Point(17, 9);
+            this.cityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.cityLabel.Location = new System.Drawing.Point(17, 31);
             this.cityLabel.Name = "cityLabel";
-            this.cityLabel.Size = new System.Drawing.Size(56, 31);
+            this.cityLabel.Size = new System.Drawing.Size(0, 25);
             this.cityLabel.TabIndex = 1;
-            this.cityLabel.Text = "city";
             // 
             // dateLabel
             // 
@@ -69,9 +67,8 @@ namespace Weather
             this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.dateLabel.Location = new System.Drawing.Point(19, 60);
             this.dateLabel.Name = "dateLabel";
-            this.dateLabel.Size = new System.Drawing.Size(50, 25);
+            this.dateLabel.Size = new System.Drawing.Size(0, 25);
             this.dateLabel.TabIndex = 2;
-            this.dateLabel.Text = "date";
             // 
             // weatherLabel
             // 
@@ -79,9 +76,8 @@ namespace Weather
             this.weatherLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.weatherLabel.Location = new System.Drawing.Point(147, 106);
             this.weatherLabel.Name = "weatherLabel";
-            this.weatherLabel.Size = new System.Drawing.Size(81, 25);
+            this.weatherLabel.Size = new System.Drawing.Size(0, 25);
             this.weatherLabel.TabIndex = 3;
-            this.weatherLabel.Text = "weather";
             // 
             // tempFeelingsLabel
             // 
@@ -89,9 +85,8 @@ namespace Weather
             this.tempFeelingsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.tempFeelingsLabel.Location = new System.Drawing.Point(12, 166);
             this.tempFeelingsLabel.Name = "tempFeelingsLabel";
-            this.tempFeelingsLabel.Size = new System.Drawing.Size(55, 25);
+            this.tempFeelingsLabel.Size = new System.Drawing.Size(0, 25);
             this.tempFeelingsLabel.TabIndex = 3;
-            this.tempFeelingsLabel.Text = "temp";
             // 
             // windLabel
             // 
@@ -99,9 +94,8 @@ namespace Weather
             this.windLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.windLabel.Location = new System.Drawing.Point(12, 204);
             this.windLabel.Name = "windLabel";
-            this.windLabel.Size = new System.Drawing.Size(52, 25);
+            this.windLabel.Size = new System.Drawing.Size(0, 25);
             this.windLabel.TabIndex = 3;
-            this.windLabel.Text = "wind";
             // 
             // pressureLabel
             // 
@@ -109,9 +103,8 @@ namespace Weather
             this.pressureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.pressureLabel.Location = new System.Drawing.Point(12, 242);
             this.pressureLabel.Name = "pressureLabel";
-            this.pressureLabel.Size = new System.Drawing.Size(88, 25);
+            this.pressureLabel.Size = new System.Drawing.Size(0, 25);
             this.pressureLabel.TabIndex = 3;
-            this.pressureLabel.Text = "pressure";
             // 
             // wetLabel
             // 
@@ -119,9 +112,8 @@ namespace Weather
             this.wetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.wetLabel.Location = new System.Drawing.Point(12, 281);
             this.wetLabel.Name = "wetLabel";
-            this.wetLabel.Size = new System.Drawing.Size(42, 25);
+            this.wetLabel.Size = new System.Drawing.Size(0, 25);
             this.wetLabel.TabIndex = 3;
-            this.wetLabel.Text = "wet";
             // 
             // activitylabel
             // 
@@ -129,18 +121,23 @@ namespace Weather
             this.activitylabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.activitylabel.Location = new System.Drawing.Point(12, 319);
             this.activitylabel.Name = "activitylabel";
-            this.activitylabel.Size = new System.Drawing.Size(71, 25);
+            this.activitylabel.Size = new System.Drawing.Size(0, 25);
             this.activitylabel.TabIndex = 3;
-            this.activitylabel.Text = "activity";
             // 
             // comboBox1
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(295, 9);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Днепр",
+            "Киев",
+            "Одесса",
+            "Львов"});
+            this.comboBox1.Location = new System.Drawing.Point(363, 12);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // waterTempLabel
@@ -149,15 +146,14 @@ namespace Weather
             this.waterTempLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.waterTempLabel.Location = new System.Drawing.Point(12, 358);
             this.waterTempLabel.Name = "waterTempLabel";
-            this.waterTempLabel.Size = new System.Drawing.Size(107, 25);
+            this.waterTempLabel.Size = new System.Drawing.Size(0, 25);
             this.waterTempLabel.TabIndex = 3;
-            this.waterTempLabel.Text = "water temp";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 403);
+            this.ClientSize = new System.Drawing.Size(496, 403);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.waterTempLabel);
             this.Controls.Add(this.activitylabel);
--- Weather/Form1.cs
+++ Weather/Form1.cs
@@ -8,9 +8,16 @@ namespace Weather
     public partial class Form1 : Form
     {
         Timer timer;
+        string html = @"https://www.gismeteo.ua/weather-dnipro-5077/now/";
+        HtmlWeb web = new HtmlWeb();
+        HtmlAgilityPack.HtmlDocument htmlDoc;
+
         public Form1()
         {
             InitializeComponent();
+
+            htmlDoc = web.Load(html);
+
             timer = new Timer();
             timer.Interval = 1000;
             timer.Tick += Timer_Tick;
@@ -24,34 +31,39 @@ namespace Weather
 
         public void LoadData()
         {
-            var html = @"https://www.gismeteo.ua/";
-            HtmlWeb web = new HtmlWeb();
-            var htmlDoc = web.Load(html);
-
-            this.cityLabel.Invoke((MethodInvoker)(() => this.cityLabel.Text = htmlDoc.DocumentNode.SelectSingleNode(@"//a[@class='link blue weather_current_link no_border']").InnerText.Trim()));
+            this.cityLabel.Invoke((MethodInvoker)(() => this.cityLabel.Text = htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='pageinfo_title index-h1']//h1").InnerText.Trim()));
 
             this.dateLabel.Invoke((MethodInvoker)(() => this.dateLabel.Text = $"{DateTime.Now.DayOfWeek}, {DateTime.Now.Day}/{DateTime.Now.Month}, {DateTime.Now.Hour}:{DateTime.Now.Minute}"));
-           
-            this.tempLabel.Invoke((MethodInvoker)(() => this.tempLabel.Text = htmlDoc.DocumentNode.SelectSingleNode(@"//span[@class='value unit unit_temperature_c']").InnerText.Trim().Replace("minus;", "-")));
-            
-            this.weatherLabel.Invoke((MethodInvoker)(() => this.weatherLabel.Text = htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='description gray']").InnerText.Trim()));
-            
-            this.tempFeelingsLabel.Invoke((MethodInvoker)(() => this.tempFeelingsLabel.Text = $"По ощущению............................................." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='ii info_value']//span").InnerText.Trim().Replace("minus;", "-")));
-            
-            this.windLabel.Invoke((MethodInvoker)(() => this.windLabel.Text = "Ветер................................................" + htmlDoc.DocumentNode.SelectSingleNode(@"//span[@class='unit unit_wind_m_s']").InnerText.Trim()));
-            
-            this.pressureLabel.Invoke((MethodInvoker)(() => this.pressureLabel.Text = "Давление..............................." + htmlDoc.DocumentNode.SelectSingleNode(@"//span[@class='unit unit_pressure_mm_hg_atm']").InnerText.Trim()));
-            
-            this.wetLabel.Invoke((MethodInvoker)(() => this.wetLabel.Text = "Влажность..............................................." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='information _additional opened']//div[2]//div[2]").InnerText.Trim().Replace("nbsp;", " ")));
-           
-            this.activitylabel.Invoke((MethodInvoker)(() => this.activitylabel.Text = "Геомагнитная активность.............." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='information _additional opened']//div[3]//div[2]").InnerText.Trim()));
-            
-            this.waterTempLabel.Invoke((MethodInvoker)(() => this.waterTempLabel.Text = "Температура воды..............................." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='information _additional opened']//div[4]//div[2]").InnerText.Trim().Replace("minus;", " ")));
+
+            this.tempLabel.Invoke((MethodInvoker)(() => this.tempLabel.Text = htmlDoc.DocumentNode.SelectSingleNode(@"//span[@class='unit unit_temperature_c']").InnerText.Trim().Replace("minus;", "-")));
+
+            this.weatherLabel.Invoke((MethodInvoker)(() => this.weatherLabel.Text = htmlDoc.DocumentNode.SelectSingleNode(@"//span[@class='tip _top _center']").InnerText.Trim()));
+
+            this.tempFeelingsLabel.Invoke((MethodInvoker)(() => this.tempFeelingsLabel.Text = $"По ощущению............................................." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='now__feel']//span[@class='unit unit_temperature_c']").InnerText.Trim().Replace("minus;", "-")));
+
+            this.windLabel.Invoke((MethodInvoker)(() => this.windLabel.Text = "Ветер................................................" + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='nowinfo__block']//div[@class='unit unit_wind_m_s']").InnerText.Trim().Replace(" ", "").Replace("\n", " ")));
+
+            this.pressureLabel.Invoke((MethodInvoker)(() => this.pressureLabel.Text = "Давление..............................." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='nowinfo__item nowinfo__item_pressure']//div[@class='unit unit_pressure_mm_hg_atm']").InnerText.Trim().Replace(" ", "").Replace("\n", " ")));
+
+            this.wetLabel.Invoke((MethodInvoker)(() => this.wetLabel.Text = "Влажность..............................................." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='nowinfo__item nowinfo__item_humidity']//div[@class='nowinfo__value']").InnerText.Trim() + "%"));
+
+            this.activitylabel.Invoke((MethodInvoker)(() => this.activitylabel.Text = "Геомагнитная активность.............." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='nowinfo__item nowinfo__item_gm']//div[@class='nowinfo__value']").InnerText.Trim() + " балл"));
+
+            this.waterTempLabel.Invoke((MethodInvoker)(() => this.waterTempLabel.Text = "Температура воды..............................." + htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='nowinfo__item nowinfo__item_water']//div[@class='nowinfo__value']").InnerText.Trim().Replace("minus;", " ")));
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if ((sender as ComboBox).SelectedIndex == 0)
+                html = @"https://www.gismeteo.ua/weather-dnipro-5077/now/";
+            else if ((sender as ComboBox).SelectedIndex == 1)
+                html = @"https://www.gismeteo.ua/weather-kyiv-4944/now/";
+            else if ((sender as ComboBox).SelectedIndex == 2)
+                html = @"https://www.gismeteo.ua/weather-odessa-4982/now/";
+            else if ((sender as ComboBox).SelectedIndex == 3)
+                html = @"https://www.gismeteo.ua/weather-lviv-4949/now/";
 
+            htmlDoc = web.Load(html);
         }
     }
 }
